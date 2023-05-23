79310a1bbafd579f72f82265f20b00711e13b155
Konstantin <34777792+Absolutkons@users.noreply.github.com>
2023-05-23 10:55:38 +0200

    Cleanup and added documentation.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-Version 1.0 was written on tiny screen with nano editor. Modifications will come soon! 
-Project data and the pictures will follow soon too!
+# Hi there, this is RasViewTV!
+Who the hell thought of that name??
+
+# What is it?
+RasViewTV is a tiny Python3 Script that runs on a Raspberry Pi, uses a cheap motion sensor and connects to a standard TV. Depending on the motion detection of the sensor it turns the connected TV on or off.
+
+# Why is it?
+Once upon a time I got my hand on a free TV and decided it would be a great idea to have it display some financial data 24/7 in my apartment. Even though the electricity prices were still cheap back in those days I decided to only activate the TV when someone is actually around to see.
+
+# How is it?
+The Raspberry Pi uses the HDMI CEC Protocol over the HDMI-Cable to control the TV.<br>
+<img src="./ressources/RasViewTV-Schematics.png" alt="Project Schematic Overview" style="height: 200px; width: auto;"/>
+
+# How can I use this in my project?
+Just do it! (Please insert Shia LaBeouf Meme)
+Hardware you need:
+- Raspberry PI (Just use one that supports HDMI-CEC)
+- Some OS on the PI (I used Raspbian (Not sure of the Version))
+- TV that supports HDMI-CEC (I think most do) --> Monitors do NOT support it!
+- Cheap motion sensor/detector from AliExpress/Amazon/Side of the Street.
+- Some cables to connect all of that.
+
+Software you need:
+- Some Python 3 (Just use a recent version)
+- Install cec-utils on os (sudo apt install cec-utils)
+- Os needs to be installed for Python
+
+You are ready to rock!
+
+# Declaration
+That's a one evening script I just used for a very short time. Just updating it now to have better documentation, because some people asked for it. I don't know if everything is still functional.
--- controlTV.py
+++ controlTV.py
@@ -2,13 +2,14 @@
 #Name: controlTv.py
 #Author: Konstantin Lausecker
 #Date: 30.10.2020
+#Modified: 23.05.2023
 #Description: This module is designed to control a tv for 24/7
-#	      usage. The TV should display a website 24/7.
-#             To save energy and ressources the tv should be
-#             turned off if no motion was detected by using a
-#             simple motion detector. 
-#Version: 1.0
-#Edits: None
+#	      		usage. The TV should display a website 24/7.
+#             	To save energy and ressources the tv should be
+#             	turned off if no motion was detected by using a
+#             	simple motion detector. 
+#Version: 1.1
+#Edits: -V1.1: Added comments and cleanup
 #Needed dependencies: Os needs to install cec-utils
 ##############################################################
 import os
@@ -16,74 +17,88 @@ import time
 from datetime import datetime, timedelta
 import RPi.GPIO as GPIO
 
+# User defined variables
 noMotionDetectedTime = 60*5 #Time the Tv will stay on while no motion is detected in s
+motionPin = 7				 #Pin which is used for motion detector --> Change
 
+# Global variables
 isSleeping = False
 lastMotionDetected = datetime.now()
 
-
+# Function to turn on the tv
 def turnOn():
-        try:
-                os.system("echo 'on 0' | cec-client -s -d 1")
-                return True
-        except Excpetion as e:
-                println("An Exception occured: " + str(e))
-                return False
+	try:
+		os.system("echo 'on 0' | cec-client -s -d 1")
+		return True
+	except Excpetion as e:
+		println("An Exception occured: " + str(e))
+		return False
 
+# Function to turn off the tv
+def turnOff():
+	try:
+		os.system("echo 'standby 0' | cec-client -s -d 1")
+		return True
+	except Excpetion as e:
+		println("An Exception occured: " + str(e))
+		return False
 
+# Interrupt function to wake up --> Triggered by motion detector
+# pin: Pin which triggered the interrupt (not used haha)
 def wakeUp(pin):
-	global lastMotionDetected
-	global isSleeping
-	if(isSleeping):
+	global lastMotionDetected	 # Get global var lastMotionDetected
+	global isSleeping			 # Get global var isSleeping
+
+	if(isSleeping): # If tv is sleeping, wake it up
 		if(turnOn()):
 			#print("AAAHAHAHH WAKEUP!!!")
 			isSleeping = False
-			lastMotionDetected = datetime.now()
+			lastMotionDetected = datetime.now() # Reset timer
 		else:
 			isSleeping = True
-	else:
-		#update Last motion time
+
+	else:			 # If tv is already awake, reset timer
 		#print("Motion detected. Resetting timer")
-		lastMotionDetected = datetime.now()
+		lastMotionDetected = datetime.now() # Reset timer
 		isSleeping = False
 
+# Function to initialize the GPIOs
 def initGpio():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
-	motionPin = 7
-	#Motiondetector input
-	GPIO.setup(motionPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.add_event_detect(motionPin, GPIO.RISING)
-	GPIO.add_event_callback(motionPin, wakeUp)
-def turnOff():
-        try:
-                os.system("echo 'standby 0' | cec-client -s -d 1")
-                return True
-        except Excpetion as e:
-                println("An Exception occured: " + str(e))
-                return False
+	GPIO.setmode(GPIO.BOARD) 	# Use board pin numbering
+	GPIO.setwarnings(False) 	# Disable warnings
+	global motionPin			# Pin used for motion detector
+	
+	GPIO.setup(motionPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set motionPin as input with pull down resistor
 
+	GPIO.add_event_detect(motionPin, GPIO.RISING) # Add interrupt to motionPin when voltage is rising
+	GPIO.add_event_callback(motionPin, wakeUp)	 # Add callback function to motionPin -> wakeup(motionPin)
+
+# Function to check if it is time to sleep
+# isSleeping: Boolean which indicates if the tv is sleeping or not
+# return: Boolean which indicates if it is time to sleep or not
 def checkToSleep(isSleeping):
-	global lastMotionDetected
+	global lastMotionDetected # Get global var lastMotionDetected
+
 	#check if it is time to sleep or not
 	if(datetime.now() > (lastMotionDetected + timedelta(0,noMotionDetectedTime))):
-		if(isSleeping == False):
-			#print("I would turn off now")
+		if(isSleeping == False): # TV is awake
 			turnOff()
 			return True
-		else:
+		else: 					 # TV already sleeps
 			#print("I am already asleep")
 			return True
 	else:
-		time.sleep(0.1)
+		time.sleep(0.1) # Wait 100ms
 		#print("I think its no time to sleep, pepe")
+
 	return False
 
+# Main function to start the program when called directly
 if __name__ == "__main__":
 	try:
-		initGpio()
-		while(True):
-			isSleeping = checkToSleep(isSleeping)
-			time.sleep(1)
+		initGpio() 	# Initialize GPIOs
+		while(True): # Main loop
+			isSleeping = checkToSleep(isSleeping) 	# Check if it is time to sleep
+			time.sleep(1)					 		# Wait 1s
 	except KeyboardInterrupt as K:
 		print(str(K))
